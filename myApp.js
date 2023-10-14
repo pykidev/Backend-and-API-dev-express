38e199e23ae8150a4faab6484a497b6adadefec6
pykidev <pykidev@gmail.com>
2023-10-14 12:27:27 +0300

    working express server challenge

--- myApp.js
+++ myApp.js
@@ -1,7 +1,10 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
-
+// console.log("Hello World");
+//methods
+app.get("/", (req, res)=>{
+    res.send("Hello Express");
+});
 
 
 
