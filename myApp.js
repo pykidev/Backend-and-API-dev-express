5f287c49205ec91cdd6487900f901a0603e0f841
pykidev <pykidev@gmail.com>
2023-10-14 12:45:28 +0300

    serve static file challenge

--- myApp.js
+++ myApp.js
@@ -1,5 +1,10 @@
 let express = require('express');
 let app = express();
+
+//serving static assets
+let staticPath = __dirname + "/public";
+//mounting the middleware to serve static files
+app.use(staticPath, express.static);
 // console.log("Hello World");
 //methods
 app.get("/", (req, res)=>{
